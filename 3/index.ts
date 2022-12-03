8a0c75201eee173266c7f0df2b0189f6377e8699
Xewol <bartekdeluga0702@gmail.com>
2022-12-03 18:21:56 +0100

    moved to everyone's favourite one liner

--- 3/index.ts
+++ 3/index.ts
@@ -4,8 +4,8 @@ import fs from 'fs'
 
 const data = fs.readFileSync('./data.txt','utf8')
 
+//*split on new line, replace mac's \r and return slices of 2 compartments 
+let splitted=data.split('\n').map((pair)=>pair.replace('\r','')).map((item)=>[item.slice(0,item.length/2),item.slice(item.length/2)])
 
-let splitted=data.split('\n')
-splitted = splitted.map((pair)=>pair.replace('\r',''))
 
 console.log(splitted)
