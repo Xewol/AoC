4d77219795bb052975894cfc2bcf43e3fea4f965
Xewol <bartekdeluga0702@gmail.com>
2022-12-08 18:47:02 +0100

    testing

--- 2022/7/a.ts
+++ 2022/7/a.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
-import _ from 'lodash'
-
+import _, { first, without } from 'lodash'
+import util from 'util'
 
 
 
@@ -14,6 +14,9 @@ interface Dir{
 const homeDir:Dir[] = []
 let data = fs.readFileSync('./data.txt','utf8').split('\n').map((element)=>element.replace('\r','')).map((element)=>element.replace('$ ',''))
 let dirHist:string[] =[]
+
+
+
 
 const cwd = ()=>dirHist[dirHist.length-1]
 
@@ -44,41 +47,61 @@ for (let [idx,command] of data.entries()){
     
 }
 
-console.log("Home dir",homeDir)
 
 for (let command of data){
     if(command.startsWith('cd')){
         cd(command.split(' ')[1])
+        continue;
     }
-
-       //i dont think if ls is needed
+    //i don't think if ls is needed
     if(command.startsWith('ls')) continue;
-
     //ls does the following
     for (let entity of homeDir){
         //! we cant check subdirectories of file so we skip
         if(!entity.name.startsWith('dir')) continue;
         //*it's a main subdirectory of '/'
-        if(entity.name === cwd()){
+        if(entity.name.split(' ')[1] === cwd()){
             if(command.startsWith('dir')){
                 //!subElements is know because its a dir not a file
                 entity.subElements!.push({name:command,subElements:[],size:0})
-           }
-           else{
-            const [size,name] =command.split(' ')
-            entity.subElements!.push({name:name,size:Number(size)})
-           }
+            }
+            else{
+                const [size,name] =command.split(' ')
+                entity.subElements!.push({name:name,size:Number(size)})
+            }
         }
         //*need to search through every folder until dir is found
         else{
-            for (let subEntity in entity.subElements){
-                
+            let foundDir = entity.subElements!.find((val)=>val.name.split(' ')[1] === cwd())
+            if(foundDir){
+                if(command.startsWith('dir')){
+                    //!subElements is know because its a dir not a file
+                    foundDir.subElements!.push({name:command,subElements:[],size:0})
+               }
+               else{
+                const [size,name] =command.split(' ')
+                foundDir.subElements!.push({name:name,size:Number(size)})
+               }
             }
         }
-        //TODO find 
     }
     
 }
 
 
-// console.log(data)
+//? :)
+const goDepth = (arr:Dir[],depth=0)=>{
+    if (!arr)return
+    for (let entity of arr){
+        
+        console.log(`${'\t'.repeat(depth)}-${entity.name} ${!entity.name.startsWith('dir')  ? `${entity.size}` : ''}`)
+        if(entity.subElements){
+            goDepth(entity.subElements,depth+1)
+        }
+    }
+
+}
+
+
+
+goDepth(homeDir)
