2b055a27e8b130e83da9a58ad7d5894dabd8cff5
Xewol <bartekdeluga0702@gmail.com>
2022-12-11 17:44:51 +0100

    still working

--- 2022/9/a.ts
+++ 2022/9/a.ts
@@ -10,26 +10,33 @@ const data = fs
 
 type Coordinate = { x: number; y: number }
 
-function getTailPosition(head: Coordinate, tail: Coordinate): Coordinate {
-  if (Math.abs(head.x - tail.x) === 2 || Math.abs(head.y - tail.y) === 2) {
-    // if the head is ever two steps directly up, down, left, or right from the tail,
-    // the tail must also move one step in that direction so it remains close enough.
-    return {
-      x: tail.x + (head.x - tail.x) / Math.abs(head.x - tail.x),
-      y: tail.y + (head.y - tail.y) / Math.abs(head.y - tail.y),
-    }
-  } else if (head.x !== tail.x && head.y !== tail.y) {
-    // Otherwise, if the head and tail aren't touching and aren't in the same row or column,
-    // the tail always moves one step diagonally to keep up.
-    return {
-      x: tail.x + (head.x - tail.x) / Math.abs(head.x - tail.x),
-      y: tail.y + (head.y - tail.y) / Math.abs(head.y - tail.y),
-    }
-  } else {
-    // If none of the above conditions are met, the tail does not move.
-    return tail
+function moveTail() {
+  const tailWatch = { ...Tail }
+  //* if the head is ever two steps directly up, down, left, or right from the tail,
+  //* the tail must also move one step in that direction so it remains close enough.
+  let xMove = Head.x - Tail.x
+  let yMove = Head.y - Tail.y
+  if (
+    (Math.abs(xMove) === 2 && yMove >= 2) ||
+    (Math.abs(yMove) === 2 && xMove >= 2)
+  ) {
+    Tail.x += xMove > 0 ? 1 : -1
+    Tail.y += yMove > 0 ? 1 : -1
+    return !_.isEqual(Tail, tailWatch) && tailMoves.push({ ...Tail })
+  }
+  if (Math.abs(xMove) === 2) {
+    Tail.x += xMove > 0 ? 1 : -1
+    return !_.isEqual(Tail, tailWatch) && tailMoves.push({ ...Tail })
+  }
+  if (Math.abs(yMove) === 2) {
+    Tail.y += yMove > 0 ? 1 : -1
+    return !_.isEqual(Tail, tailWatch) && tailMoves.push({ ...Tail })
   }
 }
+
+const Grid: string[][] = Array(8)
+  .fill('.')
+  .map(x => Array(10).fill('.'))
 
 const Head: Coordinate = { x: 0, y: 0 }
 let Tail: Coordinate = { x: 0, y: 0 }
@@ -50,13 +57,24 @@ const makeMove = (move: string) => {
       break
   }
 }
+
 const tailMoves: Coordinate[] = []
 for (const [move, value] of data) {
   for (let i = 0; i < value; i++) {
     makeMove(move as string)
-    Tail = getTailPosition(Head, Tail)
-    tailMoves.push(Tail)
+    moveTail()
   }
 }
-
 console.log(tailMoves.length)
+console.log(tailMoves)
+
+for (let move of tailMoves) {
+  Grid[move.y][move.x] = '#'
+}
+console.log(Grid.reverse().map(x => x.join('')))
+
+function test() {
+  Head.x = 3
+  Head.y = 1
+  Tail.x
+}
