0b345ad27e6868f75891960079fd8d57c9f6d9c7
Xewol <bartekdeluga0702@gmail.com>
2022-12-11 23:04:49 +0100

    day 9/a

--- 2022/9/a.ts
+++ 2022/9/a.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
-import _ from 'lodash'
+import _, { tail, uniq } from 'lodash'
+import { json } from 'stream/consumers'
 
 const data = fs
   .readFileSync('./data.txt', 'utf8')
@@ -10,71 +11,52 @@ const data = fs
 
 type Coordinate = { x: number; y: number }
 
+const Head: Coordinate = { x: 0, y: 0 }
+let Tail: Coordinate = { x: 0, y: 0 }
+const unique = new Set<string>()
+unique.add(`${0}, ${0}`)
 function moveTail() {
-  const tailWatch = { ...Tail }
+  let { x, y } = Tail
   //* if the head is ever two steps directly up, down, left, or right from the tail,
   //* the tail must also move one step in that direction so it remains close enough.
   let xMove = Head.x - Tail.x
   let yMove = Head.y - Tail.y
   if (
-    (Math.abs(xMove) === 2 && yMove >= 2) ||
-    (Math.abs(yMove) === 2 && xMove >= 2)
+    (Math.abs(xMove) === 2 && [1, 2].includes(Math.abs(yMove))) ||
+    (Math.abs(yMove) === 2 && [1, 2].includes(Math.abs(xMove)))
   ) {
-    Tail.x += xMove > 0 ? 1 : -1
-    Tail.y += yMove > 0 ? 1 : -1
-    return !_.isEqual(Tail, tailWatch) && tailMoves.push({ ...Tail })
+    x += xMove > 0 ? 1 : -1
+    y += yMove > 0 ? 1 : -1
+  } else if (Math.abs(xMove) === 2) {
+    x += xMove > 0 ? 1 : -1
+  } else if (Math.abs(yMove) === 2) {
+    y += yMove > 0 ? 1 : -1
   }
-  if (Math.abs(xMove) === 2) {
-    Tail.x += xMove > 0 ? 1 : -1
-    return !_.isEqual(Tail, tailWatch) && tailMoves.push({ ...Tail })
-  }
-  if (Math.abs(yMove) === 2) {
-    Tail.y += yMove > 0 ? 1 : -1
-    return !_.isEqual(Tail, tailWatch) && tailMoves.push({ ...Tail })
-  }
+  unique.add(`${x}, ${y}`)
+  Tail.x = x
+  Tail.y = y
 }
-
-const Grid: string[][] = Array(8)
-  .fill('.')
-  .map(x => Array(10).fill('.'))
-
-const Head: Coordinate = { x: 0, y: 0 }
-let Tail: Coordinate = { x: 0, y: 0 }
-
 const makeMove = (move: string) => {
   switch (move) {
     case 'R':
-      Head.x++
+      Head.x += 1
       break
     case 'L':
-      Head.x--
+      Head.x -= 1
       break
     case 'U':
-      Head.y++
+      Head.y += 1
       break
     case 'D':
-      Head.y--
+      Head.y -= 1
       break
   }
 }
-
-const tailMoves: Coordinate[] = []
 for (const [move, value] of data) {
   for (let i = 0; i < value; i++) {
     makeMove(move as string)
     moveTail()
   }
 }
-console.log(tailMoves.length)
-console.log(tailMoves)
 
-for (let move of tailMoves) {
-  Grid[move.y][move.x] = '#'
-}
-console.log(Grid.reverse().map(x => x.join('')))
-
-function test() {
-  Head.x = 3
-  Head.y = 1
-  Tail.x
-}
+console.log(unique.size)
